b1dd9f95c281f90cadcb865a114f8c381a316d38
sooking87 <sksohn01@naver.com>
2023-05-31 23:09:27 +0900

    HLL 끝!!!!!!!!tlqk

--- ESQL/report.md
+++ ESQL/report.md
@@ -7,6 +7,7 @@ CREATE SCHEMA `Product-Purchase-History-Management` ;
 
 CREATE TABLE `product-purchase-history-management`.`customer` (
   `CusID` VARCHAR(45) NOT NULL,
+  `CusPW` VARCHAR(45) NOT NULL,
   `CusNAME` VARCHAR(45) NOT NULL,
   `AGE` INT NOT NULL,
   `PhoneNUM` CHAR(15) NULL,
@@ -14,11 +15,11 @@ CREATE TABLE `product-purchase-history-management`.`customer` (
   `SEX` CHAR(10) NOT NULL,
   PRIMARY KEY (`CusID`));
 
-insert into `Product-Purchase-History-Management`.`customer` values('C1', 'Smith', 20, '011-1111-1111', '영등포구', 'MALE');
-insert into `Product-Purchase-History-Management`.`customer` values('C2', 'Jones', 18, '011-2222-2222', '용산구', 'MALE');
-insert into `Product-Purchase-History-Management`.`customer` values('C3', 'Blake', 30, '011-3333-3333', '분당구', 'FEMALE');
-insert into `Product-Purchase-History-Management`.`customer` values('C4', 'Clark', 20, '011-4444-4444', '분당구', 'FEMALE');
-insert into `Product-Purchase-History-Management`.`customer` values('sksohn01', 'sooking87', 23, '010-3809-7668', '용산구', 'FEMALE');
+insert into `Product-Purchase-History-Management`.`customer` values('C1', '111', 'Smith', 20, '011-1111-1111', '영등포구', 'MALE');
+insert into `Product-Purchase-History-Management`.`customer` values('C2', '222', 'Jones', 18, '011-2222-2222', '용산구', 'MALE');
+insert into `Product-Purchase-History-Management`.`customer` values('C3', '333', 'Blake', 30, '011-3333-3333', '분당구', 'FEMALE');
+insert into `Product-Purchase-History-Management`.`customer` values('C4', '444', 'Clark', 20, '011-4444-4444', '분당구', 'FEMALE');
+insert into `Product-Purchase-History-Management`.`customer` values('sksohn01', 'sks0hn01!!', 'sooking87', 23, '010-3809-7668', '용산구', 'FEMALE');
 ```
 
 ### purchase table
@@ -31,14 +32,13 @@ CREATE SCHEMA `Product-Purchase-History-Management` ;
 CREATE TABLE `product-purchase-history-management`.`purchase` (
   `PNO` CHAR(15) NOT NULL,
   `CusID` VARCHAR(45) NOT NULL,
-  `SNO` CHAR(5) NOT NULL,
   `DATE` CHAR(20) NOT NULL);
 
 
-insert into `Product-Purchase-History-Management`.`purchase` values('00-230524-2243', 'C1', 'S1', '2023-05-24-10:38');
-insert into `Product-Purchase-History-Management`.`purchase` values('01-230522-1015', 'C2', 'S2', '2023-05-22-22:20');
-insert into `Product-Purchase-History-Management`.`purchase` values('01-230522-1015', 'sksohn01', 'S1', '2023-05-27-23:45');
-insert into `Product-Purchase-History-Management`.`purchase` values('00-230525-2354', 'sksohn01', 'S1', '2023-05-27-23:50');
+insert into `Product-Purchase-History-Management`.`purchase` values('00-230524-2243', 'C1', '2023-05-24-10:38');
+insert into `Product-Purchase-History-Management`.`purchase` values('01-230522-1015', 'C2', '2023-05-22-22:20');
+insert into `Product-Purchase-History-Management`.`purchase` values('01-230522-1015', 'sksohn01', '2023-05-27-23:45');
+insert into `Product-Purchase-History-Management`.`purchase` values('00-230525-2354', 'sksohn01', '2023-05-27-23:50');
 ```
 
 ### product table
@@ -51,34 +51,35 @@ CREATE TABLE `product-purchase-history-management`.`product` (
   `PINFO` VARCHAR(45) NOT NULL,
   `PRICE` INT NOT NULL,
   `CNT` INT NOT NULL,
+  `PNAME` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`PNO`),
   CHECK (`CNT` >= 0 and `CNT` <= 10000)
 );
 
 -- 뒤에 - 붙히고 등록 년월일시분 넣기
 -- 상의: 00
-insert into `Product-Purchase-History-Management`.`product` values('00-230523-1134', 'TOP', 15000, 32);
-insert into `Product-Purchase-History-Management`.`product` values('00-230524-2239', 'TOP', 12000, 60);
-insert into `Product-Purchase-History-Management`.`product` values('00-230524-2243', 'TOP', 23000, 45);
-insert into `Product-Purchase-History-Management`.`product` values('00-230525-2348', 'TOP', 32000, 16);
-insert into `Product-Purchase-History-Management`.`product` values('00-230525-2354', 'TOP', 16000, 26);
+insert into `Product-Purchase-History-Management`.`product` values('00-230523-1134', 'TOP', 15000, 32, '하트넥 셔링 골지 크롭 반팔 니트');
+insert into `Product-Purchase-History-Management`.`product` values('00-230524-2239', 'TOP', 12000, 60, '셔링 단가라 크롭 반팔');
+insert into `Product-Purchase-History-Management`.`product` values('00-230524-2243', 'TOP', 23000, 45, '시스루 오버핏 셔츠');
+insert into `Product-Purchase-History-Management`.`product` values('00-230525-2348', 'TOP', 32000, 16, '홀터 니트');
+insert into `Product-Purchase-History-Management`.`product` values('00-230525-2354', 'TOP', 16000, 26, '레이스 단추 브이넥');
 -- 하의: 01
-insert into `Product-Purchase-History-Management`.`product` values('01-230519-1028', 'BOTTOM', 23000, 43);
-insert into `Product-Purchase-History-Management`.`product` values('01-230519-1130', 'BOTTOM', 35000, 72);
-insert into `Product-Purchase-History-Management`.`product` values('01-230521-2334', 'BOTTOM', 43000, 21);
-insert into `Product-Purchase-History-Management`.`product` values('01-230521-2343', 'BOTTOM', 32000, 16);
-insert into `Product-Purchase-History-Management`.`product` values('01-230522-1015', 'BOTTOM', 20000, 36);
-insert into `Product-Purchase-History-Management`.`product` values('01-230523-1420', 'BOTTOM', 25000, 32);
-insert into `Product-Purchase-History-Management`.`product` values('01-230523-1424', 'BOTTOM', 28000, 12);
-insert into `Product-Purchase-History-Management`.`product` values('01-230524-2215', 'BOTTOM', 20000, 82);
+insert into `Product-Purchase-History-Management`.`product` values('01-230519-1028', 'BOTTOM', 23000, 43, '롱 와이드 팬츠');
+insert into `Product-Purchase-History-Management`.`product` values('01-230519-1130', 'BOTTOM', 35000, 72, '쿨링 밴딩 팬츠');
+insert into `Product-Purchase-History-Management`.`product` values('01-230521-2334', 'BOTTOM', 43000, 21, '허리 라벨 일자핏 팬츠');
+insert into `Product-Purchase-History-Management`.`product` values('01-230521-2343', 'BOTTOM', 32000, 16, '포켓 와이드 트랙 팬츠');
+insert into `Product-Purchase-History-Management`.`product` values('01-230522-1015', 'BOTTOM', 20000, 36, '투웨이 와이드 카고 팬츠');
+insert into `Product-Purchase-History-Management`.`product` values('01-230523-1420', 'BOTTOM', 25000, 32, '빈티지 워싱 데님 숏팬츠');
+insert into `Product-Purchase-History-Management`.`product` values('01-230523-1424', 'BOTTOM', 28000, 12, '하이웨스트 청반바지');
+insert into `Product-Purchase-History-Management`.`product` values('01-230524-2215', 'BOTTOM', 20000, 82, '알렉산더 맥퀸 반바지');
 -- 신발: 02
-insert into `Product-Purchase-History-Management`.`product` values('02-230523-1134', 'SHOES', 45000, 32);
-insert into `Product-Purchase-History-Management`.`product` values('02-230524-2239', 'SHOES', 32000, 42);
-insert into `Product-Purchase-History-Management`.`product` values('02-230524-2243', 'SHOES', 23000, 37);
-insert into `Product-Purchase-History-Management`.`product` values('02-230525-2348', 'SHOES', 56000, 16);
+insert into `Product-Purchase-History-Management`.`product` values('02-230523-1134', 'SHOES', 45000, 32, '통굽 뮬');
+insert into `Product-Purchase-History-Management`.`product` values('02-230524-2239', 'SHOES', 32000, 42, '샘플 스트랩 밴딩 샌들');
+insert into `Product-Purchase-History-Management`.`product` values('02-230524-2243', 'SHOES', 23000, 37, '밴딩 통굽 샌들');
+insert into `Product-Purchase-History-Management`.`product` values('02-230525-2348', 'SHOES', 56000, 16, '초경량 쪼리');
 -- 패션소품: 03
-insert into `Product-Purchase-History-Management`.`product` values('03-230522-1023', 'ACC', 8000, 14);
-insert into `Product-Purchase-History-Management`.`product` values('03-230525-1148', 'ACC', 12000, 16);
+insert into `Product-Purchase-History-Management`.`product` values('03-230522-1023', 'ACC', 8000, 14, '두줄 레이어드 목걸이');
+insert into `Product-Purchase-History-Management`.`product` values('03-230525-1148', 'ACC', 12000, 16, '하트 오픈 링');
 ```
 
 ### cart table
@@ -86,7 +87,7 @@ insert into `Product-Purchase-History-Management`.`product` values('03-230525-11
 ```sql
 CREATE SCHEMA `Product-Purchase-History-Management` ;
 
-CREATE TABLE `suppliers-parts-projects`.`cart` (
+CREATE TABLE `Product-Purchase-History-Management`.`cart` (
   `CusID` VARCHAR(45) NOT NULL,
   `PNO` CHAR(15) NOT NULL,
   `InDATE` CHAR(20) NOT NULL);
@@ -106,6 +107,26 @@ insert into `Product-Purchase-History-Management`.`cart` values('sksohn01', '02-
 insert into `Product-Purchase-History-Management`.`cart` values('sksohn01', '03-230525-1148', '2023-05-27-23:53');
 ```
 
+### deliveryperson TABLE
+
+```sql
+CREATE TABLE `product-purchase-history-management`.`deliveryperson` (
+  `DNO` CHAR(5) NOT NULL,
+  `DNAME` VARCHAR(45) NOT NULL,
+  `DPhoneNUM` CHAR(15) NOT NULL,
+  PRIMARY KEY (`DNO`));
+
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D111', '돌비', '010-0000-1111');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D222', '한씨', '010-1111-2222');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D333', '손씨', '010-2222-3333');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D444', '윤씨', '010-3333-4444');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D555', '이씨', '010-4444-5555');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D666', '최씨', '010-5555-6666');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D777', '동꼬', '010-6666-7777');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D888', '아스라', '010-7777-8888');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D999', '레몬과자', '010-8888-9999');
+insert into `Product-Purchase-History-Management`.`deliveryperson` values('D000', '백뭉', '010-9999-0000');
+```
 ## HLL에서 필요한 API
 
 - show_product_list(): 구매 가능한 상품을 반환하는 함수
@@ -113,3 +134,13 @@ insert into `Product-Purchase-History-Management`.`cart` values('sksohn01', '03-
 - customer_info(): 회원 정보와 과거 주문 내역을 확인하는 함수
 - sign_up(): 회원가입을 진행하는 함수
 - resign(): 회원탈퇴를 진행하는 함수
+
+```sql
+-- UPDATE purchase SET DNO = 'D333' WHERE CusID = 'sksohn01';
+
+-- DELETE FROM cart WHERE cart.PNO='00-230525-2348' AND cart.CusID='sksohn01'
+
+-- UPDATE product SET CNT=20 WHERE PNO='00-230523-1134'
+
+-- INSERT INTO `Product-Purchase-History-Management`.`purchase` (`PNO`, `CusID`, `DNO`, `DATE`) VALUES(%s %s %s %s)
+```
--- ESQL/sale_api.py
+++ ESQL/sale_api.py
@@ -1,12 +1,12 @@
 import pymysql
 import pandas as pd
 from tabulate import tabulate
-
+import re
+from random import randint
+import datetime
 
 def show_product_list():
     # 구매 가능한 상품 보여주기
-    print('구매 가능 내역입니다.')
-    print('---------------------------------')
     # cursor를 이용해서 sql문 실행
     db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
                          charset='utf8')
@@ -17,8 +17,8 @@ def show_product_list():
     cur.execute(top)
     rows = cur.fetchall()
     products = pd.DataFrame(rows)
-
-    print(tabulate(products, headers='keys',
+    headers = ['상품명', '가격']
+    print(tabulate(products, headers=headers, 
           tablefmt='psql', showindex=True), '\n')
 
     # BOTTOM 내역 출력하기
@@ -27,7 +27,7 @@ def show_product_list():
     cur.execute(bottom)
     rows = cur.fetchall()
     products = pd.DataFrame(rows)
-    print(tabulate(products,
+    print(tabulate(products, headers=headers,
           tablefmt='psql', showindex=True), '\n')
 
     # SHOES 내역 출력하기
@@ -37,7 +37,7 @@ def show_product_list():
     rows = cur.fetchall()
     products = pd.DataFrame(rows)
 
-    print(tabulate(products,
+    print(tabulate(products, headers=headers,
           tablefmt='psql', showindex=True), '\n')
 
     # ACC 내역 출력하기
@@ -46,22 +46,21 @@ def show_product_list():
     cur.execute(acc)
     rows = cur.fetchall()
     products = pd.DataFrame(rows)
-    print(tabulate(products,
+    print(tabulate(products, headers=headers,
           tablefmt='psql', showindex=True), '\n')
 
     cur.close()
 
-
 def order():
     # 주문 처리 함수
     print('로그인창으로 넘어갑니다.')
-    cusID = sign_in()
-    if cusID == -1:
+    cusID, cusPW = sign_in()
+    if cusID == -1 and cusPW == -1:
         return
     show_product_list()
     # cursor를 이용해서 sql문 실행
     db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
-                         charset='utf8')
+                         charset='utf8', autocommit=True)
     cur = db.cursor()
     sql = 'SELECT DISTINCT PINFO FROM product'
     cur.execute(sql)
@@ -75,44 +74,201 @@ def order():
         cate, num = input('어떤 상품을 구매하시겠습니까?(카테고리 제품번호): ').split()
         if cate in categories:
             # ROW_NUMBER를 이용해서 사용자가 원하는 카테고리의 번호만 입력으로도 주문 가능 상품인지 아닌지 조회하는 쿼리
-            sql = 'SELECT * FROM(SELECT ROW_NUMBER() OVER(ORDER BY PRICE) AS num, PNO, PNAME, PRICE FROM product WHERE PINFO = "' + \
-                cate + '") temp WHERE temp.num = ' + num
-            cur.execute(sql)
+            sql = 'SELECT * FROM(SELECT ROW_NUMBER() OVER(ORDER BY PRICE) AS num, PNO, PNAME, PRICE, CNT FROM product WHERE PINFO=%s) temp WHERE temp.num=%s'
+            val = (cate, num)
+            cur.execute(sql, val)
 
             rows = cur.fetchall()
+            cnt = rows[0][4]
+            # CNT가 1이상이므로 주문이 가능하다면
             if (rows):
-                product = pd.DataFrame(rows)
-                print(tabulate(product,
-                               tablefmt='psql', showindex=False))
-                print('상품 주문이 완료되었습니다.')
-                # 해당 id의 사용자의 purchase에는 추가
-                # product의 CNT는 -1
-                # 카트에 있으면 지우기 없으면 걍 넘어가
+                # product = pd.DataFrame(rows)
+                # print(tabulate(product,
+                #                tablefmt='psql', showindex=False))
+                ordered_pno = rows[0][1]
+                # print('상품 주문이 완료되었습니다.')
+
+                # 주문한 상품(product TABLE)을 상품내역 리스트(purchase TABLE)에 추가하기
+                # 카트(cart TABLE)에 해당 물품이 있다면 주문했으므로 삭제하기
+
+                # 주문한 상품 아이디와 고객 아이디를 바탕으로 카트에 존재를 하는지
+                sql2 = 'SELECT * FROM cart, customer, product WHERE customer.CusID = cart.CusID AND product.PNO = cart.PNO AND product.PNO=%s AND customer.CusID=%s'
+                val = (ordered_pno, cusID)
+                cur.execute(sql2, val)
+                
+                is_in_cart = cur.fetchall()
+                # print(is_in_cart)
+                # 주문하려는 상품이 카트에 존재한다면
+                if is_in_cart:
+                    # cart TABLE에 있는 상품은 DELETE
+                    # print("조회한 결과: ")
+                    # print(tabulate(product,
+                    #             tablefmt='psql', showindex=False))
+                    sql = 'DELETE FROM cart WHERE cart.PNO=%s AND cart.CusID=%s'
+                    val = (ordered_pno, cusID)
+                    cur.execute(sql, val)
+
+                    # print('cart 정보 삭제 완료')
+                # product TABLE에서 수량(CNT) -1하기
+                ## cnt -= 1
+                ## update = 'UPDATE product SET CNT=%s WHERE PNO=%s'
+                ## val = (cnt, ordered_pno)
+                ## cur.execute(update, val)
+                # print('product CNT 업데이트 완료')
+                
+                # 주문하려는 상품이 카트에 존재하지 않다면 그냥 사용자 정보만 보여주면 됨.
+                # purchase TABLE에는 INSERT
+                # 배달 기사의 경우 랜덤으로 배정
+                delivery_person = randint(1, 10)
+                assign_dp = 'SELECT DNO FROM(SELECT ROW_NUMBER() OVER(ORDER BY DNO) AS num, DNO FROM deliveryperson) temp WHERE temp.num=%s'
+                cur.execute(assign_dp, delivery_person)
+
+                rows = cur.fetchall()
+                # print(rows[0][0], '택배 기사 배정 완료')
+                charge = rows[0][0]
+                # DATE 칼럼의 경우는 현재 INSERT 하는 시간 기준
+                now = datetime.datetime.now()
+                date = str(now.strftime('%Y-%m-%d-%H:%M'))
+
+                insert = 'INSERT INTO `Product-Purchase-History-Management`.`purchase` (`PNO`, `CusID`, `DNO`, `DATE`) VALUES(%s, %s, %s, %s)'
+                val = (ordered_pno, cusID, charge, date)
+                cur.execute(insert, val)
+                # print('purchase TABLE update 완료')
+
+                # 주문 완료 후 성함, 의류 카테코리, 가격, 상품명, 주문 시간, 배당된 택배기사 번호를 출력하기 위한 쿼리문
+                notice = 'SELECT customer.CusNAME, product.PINFO, product.PRICE, product.PNAME, purchase.DATE, deliveryperson.DNAME FROM customer, product, purchase, deliveryperson WHERE customer.CusID=purchase.CusID AND purchase.PNO=product.PNO AND purchase.DNO=deliveryperson.DNO AND customer.CusID=%s AND product.PNO=%s AND deliveryperson.DNO=%s'
+                val = (cusID, ordered_pno, charge)
+                cur.execute(notice, val)
+
+                rows = cur.fetchall()
+                result = pd.DataFrame(rows)
+                print('\n%s님 상품 주문이 완료되었습니다.\n' %cusID)
+                print(tabulate(result, 
+                        tablefmt='psql', showindex=False))
+                print('감사합니다.')
                 break
             else:
                 print('해당 상품은 존재하지 않습니다. 다시 입력해주세요.')
         else:
             print('입력한 카테고리가 존재하지 않습니다. 다시 입력해주세요.')
-
+    cur.close()
 
 def cart():
     # 카트에 상품 넣기
-    print('cart()')
+    print('로그인창으로 넘어갑니다.')
+    cusID, cusPW = sign_in()
+    if cusID == -1 and cusPW == -1:
+        return
+    show_product_list()
+    # cursor를 이용해서 sql문 실행
+    db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
+                         charset='utf8', autocommit=True)
+    cur = db.cursor()
+    sql = 'SELECT DISTINCT PINFO FROM product'
+    cur.execute(sql)
+
+    rows = cur.fetchall()
+    categories = [rows[i][0] for i in range(len(rows))]
+
+    while True:
+        print('카테고리: ', end=' ')
+        print(categories)
+        cate, num = input('어떤 상품을 카트에 넣겠습니까?(카테고리 제품번호): ').split()
+
+        if cate in categories:
+            # ROW_NUMBER를 이용해서 사용자가 원하는 카테고리의 번호만 입력으로도 주문 가능 상품인지 아닌지 조회하는 쿼리
+            sql = 'SELECT * FROM(SELECT RANK() OVER(ORDER BY PRICE) AS num, PNO, PNAME, PRICE, CNT FROM product WHERE PINFO=%s) temp WHERE temp.num=%s'
+            val = (cate, num)
+            print("tlqkf")
+            cur.execute(sql, val)
+
+            rows = cur.fetchall()
+            # CNT가 1이상이므로 카트에 넣기
+            if (rows):
+                cart_pno = rows[0][1]
+                
+                # DATE 칼럼의 경우는 현재 INSERT 하는 시간 기준
+                now = datetime.datetime.now()
+                date = str(now.strftime('%Y-%m-%d-%H:%M'))
+
+                insert = 'INSERT INTO `Product-Purchase-History-Management`.`cart` (`CusID`, `PNO`, `InDATE`) VALUES(%s, %s, %s)'
+                val = (cusID, cart_pno, date)
+                cur.execute(insert, val)
+
+                print('\n%s님 상품을 카트에 담았습니다.\n' %cusID)
+                break
+            else:
+                print('해당 상품은 존재하지 않습니다. 다시 입력해주세요.')
+        else:
+            print('입력한 카테고리가 존재하지 않습니다. 다시 입력해주세요.')
+    cur.close()
 
 
 def customer_info():
     # 상품 내역 조회
-    print('customer_info()')
-
-
-def sign_up():
-    # 회원 가입
-    print('sign_up()')
+    print('\n로그인창으로 넘어갑니다.\n')
+    cusID, cusPW = sign_in()
+    if cusID == -1 and cusPW == -1:
+        return
     db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
                          charset='utf8')
     cur = db.cursor()
+    sql = "SELECT PINFO, PNAME, PRICE, DATE FROM product, customer, purchase WHERE customer.CusID = purchase.CusID AND product.PNO = purchase.PNO AND customer.CusID=%s AND CusPW=%s"
+    val = (cusID, cusPW)
+    cur.execute(sql, val)
+    rows = cur.fetchall()
+    products = pd.DataFrame(rows)
+
+    print("%s님의 주문 내역 조회 결과입니다." %cusID)
+    print(tabulate(products, headers=('카테고리', '상품명', '가격', '주문 날짜'),
+          tablefmt='psql', showindex=False), '\n')
+    cur.close()
+
+def check_cart():
+    # 카트 내역 조회
+    print('\n로그인창으로 넘어갑니다.\n')
+    cusID, cusPW = sign_in()
+    if cusID == -1 and cusPW == -1:
+        return
+    db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
+                         charset='utf8')
+    cur = db.cursor()
+    sql = "SELECT PINFO, PNAME, PRICE, InDATE FROM product, customer, cart WHERE customer.CusID = cart.CusID AND product.PNO = cart.PNO AND customer.CusID=%s AND customer.CusPW=%s"
+    val = (cusID, cusPW)
+    cur.execute(sql, val)
+    rows = cur.fetchall()
+    products = pd.DataFrame(rows)
+
+    print("%s님의 카트 내역 조회 결과입니다." %cusID)
+    print(tabulate(products, headers=('카테고리', '상품명', '가격', '넣은 날짜'),
+          tablefmt='psql', showindex=False), '\n')
+    cur.close()
+
+def format_phone_number(input):
+    # xxx-xxxx-xxxx 형식으로 전화번호를 세팅해주는 함수
+    phone_num = re.sub(r'[^0-9]', '', input)
+    result = ""
+    length = len(phone_num)
+    
+    if length == 8:
+        result = re.sub(r'(\d{4})(\d{4})', r'\1-\2', phone_num)
+    elif phone_num.startswith("02") and (length == 9 or length == 10):
+        result = re.sub(r'(\d{2})(\d{3,4})(\d{4})', r'\1-\2-\3', phone_num)
+    elif not phone_num.startswith("02") and (length == 10 or length == 11):
+        result = re.sub(r'(\d{3})(\d{3,4})(\d{4})', r'\1-\2-\3', phone_num)
+    else:
+        result = None
+
+    return result
+
+def sign_up():
+    # 회원 가입
+    db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
+                         charset='utf8', autocommit=True)
+    cur = db.cursor()
 
     newID = input('[필수] 사용할 ID를 입력해주세요: ')
+    newPW = input('[필수] 사용자의 Password를 입력해주세요: ')
     newNAME = input('[필수] 사용자의 이름을 입력해주세요: ')
     newAGE = int(input('[필수] 사용자의 나이를 입력해주세요: '))
     newSEX = int(input('[필수] 성별을 입력해주세요. (남(0) / 여(1)): '))
@@ -124,14 +280,29 @@ def sign_up():
         SEX = 'MALE'
     elif newSEX == 1:
         SEX = 'FEMALE'
+    
+    phone_num = ''
+    if newPhoneNUM != '':
+        phone_num = format_phone_number(newPhoneNUM)
+    else:
+        phone_num = None
 
-    sql = 'insert into `Product-Purchase-History-Management` `customer` values (%s %s %d %s %s %s)'
-    val = (newID, newNAME, newAGE, newPhoneNUM, newCITY, SEX)
+    sql = 'SELECT * FROM `customer` WHERE CusID=%s AND CusPW=%s'
+    val = (newID, newPW)
     cur.execute(sql, val)
     rows = cur.fetchall()
 
-    print('\n가입이 완료되었습니다.\n')
+    if (rows):
+        print('\n이미 존재하는 ID입니다.\n')
+    else:
+        sql = 'INSERT INTO `Product-Purchase-History-Management`.`customer` (`CusID`, `CusPW`, `CusNAME`, `AGE`, `PhoneNUM`, `CITY`, `SEX`) VALUES(%s, %s, %s, %s, %s, %s, %s)'
+        # INSERT INTO `Product-Purchase-History-Management`.`customer` (`CusID`, `CusNAME`, `AGE`, `PhoneNUM`, `CITY`, `SEX`) VALUES('s', 'sd', 23, '010-1111-1113', '분당구', 'FEMALE')
+        val = (newID, newPW, newNAME, newAGE, phone_num, newCITY, SEX)
+        cur.execute(sql, val)
+        rows = cur.fetchall()
 
+        print('\n가입이 완료되었습니다.\n')
+    cur.close()
 
 def sign_in():
     # 로그인 하기
@@ -139,26 +310,79 @@ def sign_in():
     db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
                          charset='utf8')
     cur = db.cursor()
-    sql = 'SELECT CusID FROM customer'
-    cur.execute(sql)
+    
+    cusID = ''
+    cusPW = ''
+    cusID = input('사용자 ID를 입력해주세요: ')
+    
+    sql = 'SELECT * FROM customer WHERE CusID=%s'
+    cur.execute(sql, cusID)
+
+    pass_ID = cur.fetchall()
+    if pass_ID:
+        while True:
+            cusPW = input('사용자 PW를 입력해주세요: ')
+            sql = 'SELECT * FROM customer WHERE CusID=%s AND CusPW=%s'
+            personal = (cusID, cusPW)
+            cur.execute(sql, personal)
+            pass_PW = cur.fetchall()
+            if pass_PW:
+                print('\n로그인 되었습니다.\n')
+                break
+            else:
+                print('\n비밀번호가 틀렸습니다. 다시 입력해주세요.\n')
+        return cusID, cusPW
+    else:
+        print('\n사용자의 ID가 회원 가입이 되지 않았습니다. 회원 가입 후 이용해주시기 바랍니다.')
+        return -1, -1
+    cur.close()
+
+def check_pw():
+    # 비밀번호 조회하기
+    db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
+                         charset='utf8')
+    cur = db.cursor()
+
+    id = input('비밀번호를 조회할 [아이디]를 입력해주세요: ')
+    sql = 'SELECT CusPW FROM customer WHERE CusID=%s'
+    cur.execute(sql, id)
 
     rows = cur.fetchall()
-    ids = [rows[i][0] for i in range(len(rows))]
-    cusID = ''
 
-    while True:
-        cusID = input('사용자 ID를 입력해주세요: ')
-        if cusID in ids:
-            print('로그인 되었습니다.')
-            return cusID
-        else:
-            print('사용자의 ID가 회원 가입이 되지 않았습니다. 회원 가입 후 이용해주시기 바랍니다.')
-            return -1
+    if rows:
+        print('\n%s님의 아이디는 %s입니다.\n' %(id, rows[0][0]))
+    else:
+        print('\n존재하지 않는 아이디입니다. 회원 가입해주세요\n')
+    cur.close()
+
 
 
 def resign():
     # 회원 탈퇴
-    print('resign()')
+    print('로그인창으로 넘어갑니다.')
+    cusID, cusPW = sign_in()
+    if cusID == -1 and cusPW == -1:
+        return
+    
+    # cursor를 이용해서 sql문 실행
+    db = pymysql.connect(host='localhost', user='root', password='sks0hn01!!', db='product-purchase-history-management',
+                         charset='utf8', autocommit=True)
+    cur = db.cursor()
+    yes = ['y', 'Y', 'yes', 'Yes', 'YES']
+    no = ['n', 'N', 'no', 'No', 'NO']
+    while True:
+        ans = input('정말 회원 탈퇴를 하시겠습니까?(y/n): ')
+        if ans in yes:
+            sql = 'DELETE FROM customer WHERE CusID=%s AND CusPW=%s'
+            val = (cusID, cusPW)
+            cur.execute(sql, val)
+
+            print('\n회원 탈퇴가 완료되었습니다.\n')
+        elif ans in no:
+            print('\n회원 탈퇴를 취소합니다.\n')
+        break
+    cur.close()        
+        
 
 
 def start():
@@ -166,9 +390,12 @@ def start():
     print('메뉴를 선택해주세요.')
     print('1. 회원 가입하기')
     print('2. 회원 탈퇴하기')
-    print('3. 구매 가능한 상품 내역 보기')
-    print('4. 상품 주문하기')
-    print('5. 카트 확인하기')
+    print('3. 회원 조회하기')
+    print('4. 구매 가능한 상품 내역 보기')
+    print('5. 상품 주문하기')
+    print('6. 카트에 상품 넣기')
+    print('7. 주문 내역 조회하기')
+    print('8. 카트 내역 조회하기')
     print('0. 종료')
     print('\n')
 
@@ -176,9 +403,7 @@ def start():
 
     return menu
 
-
 # show_product_list()
-
 while True:
     menu = start()
 
@@ -187,11 +412,17 @@ while True:
     elif menu == '2':
         resign()
     elif menu == '3':
-        show_product_list()
+        check_pw()
     elif menu == '4':
-        order()
+        show_product_list()
     elif menu == '5':
+        order()
+    elif menu == '6':
         cart()
+    elif menu == '7':
+        customer_info()
+    elif menu == '8':
+        check_cart()
     elif menu == '0':
-        print('서비스를 이용해주셔서 감사합니다.')
+        print('\n서비스를 이용해주셔서 감사합니다.\n')
         break
